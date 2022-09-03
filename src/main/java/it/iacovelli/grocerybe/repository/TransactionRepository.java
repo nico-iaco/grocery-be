112af81567c96a680742deef1dea0e6c1d6fa78e
Nicola Iacovelli <nicolaiacovelli98@gmail.com>
2022-09-03 12:09:14 +0200

    Downgraded spring boot to 2.7 to use spring native

--- src/main/java/it/iacovelli/grocerybe/repository/TransactionRepository.java
+++ src/main/java/it/iacovelli/grocerybe/repository/TransactionRepository.java
@@ -4,13 +4,11 @@ import it.iacovelli.grocerybe.model.Item;
 import it.iacovelli.grocerybe.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
 
     @Query("SELECT SUM(t.quantity) from Transaction t where t.item = :item")
