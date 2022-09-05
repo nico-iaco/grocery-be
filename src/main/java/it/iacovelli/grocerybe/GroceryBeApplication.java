11dc58130f5368874405a8c1e37bb6837b0e5cb8
Nicola Iacovelli <nicolaiacovelli98@gmail.com>
2022-09-05 13:19:17 +0200

    Added swagger

--- src/main/java/it/iacovelli/grocerybe/GroceryBeApplication.java
+++ src/main/java/it/iacovelli/grocerybe/GroceryBeApplication.java
@@ -1,9 +1,15 @@
 package it.iacovelli.grocerybe;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication( proxyBeanMethods = false )
+@OpenAPIDefinition(info = @Info(title = "Grocery API", version = "1.0.0",
+        description = "Api of an app to avoid food waste"),
+        servers = { @Server(url = "/", description = "Default URL")})
 public class GroceryBeApplication {
 
     public static void main(String[] args) {
