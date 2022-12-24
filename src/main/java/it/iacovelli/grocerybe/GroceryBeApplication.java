82f9e9a0c2cb40be99a691a42f0fb812978e8ddb
Nicola Iacovelli <nicolaiacovelli98@gmail.com>
2022-12-24 15:35:12 +0100

    WIP: Authentication for food-details-integrator-be-service 🚀⚿

--- src/main/java/it/iacovelli/grocerybe/GroceryBeApplication.java
+++ src/main/java/it/iacovelli/grocerybe/GroceryBeApplication.java
@@ -1,18 +1,14 @@
 package it.iacovelli.grocerybe;
 
-import com.google.auth.oauth2.GoogleCredentials;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
 import java.time.Duration;
 
 @SpringBootApplication(proxyBeanMethods = false)
@@ -21,22 +17,11 @@ import java.time.Duration;
         servers = {@Server(url = "/", description = "Default URL")})
 public class GroceryBeApplication {
 
-    private static final Log LOGGER = LogFactory.getLog(GroceryBeApplication.class);
-
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        String accessToken = null;
-        try {
-            GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
-            credentials.refreshIfExpired();
-            accessToken = credentials.getAccessToken().getTokenValue();
-        } catch (IOException e) {
-            LOGGER.error("Error while getting access token", e);
-        }
         return builder
                 .setConnectTimeout(Duration.ofSeconds(7))
                 .setReadTimeout(Duration.ofSeconds(7))
-                .defaultHeader("Authorization", "Bearer " + accessToken)
                 .build();
     }
 
--- src/main/java/it/iacovelli/grocerybe/service/impl/FoodDetailsIntegratorServiceImpl.java
+++ src/main/java/it/iacovelli/grocerybe/service/impl/FoodDetailsIntegratorServiceImpl.java
@@ -1,5 +1,6 @@
 package it.iacovelli.grocerybe.service.impl;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import it.iacovelli.grocerybe.exception.FoodDetailsNotAvailableException;
 import it.iacovelli.grocerybe.model.dto.FoodDetailDto;
 import it.iacovelli.grocerybe.service.FoodDetailsIntegratorService;
@@ -9,8 +10,14 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
@@ -32,22 +39,51 @@ public class FoodDetailsIntegratorServiceImpl implements FoodDetailsIntegratorSe
     public FoodDetailDto getFoodDetails(String barcode) {
         CircuitBreaker foodDetails = circuitBreakerFactory.create("foodDetails");
         LOGGER.info("Calling food details integrator BE with endpoint: " + foodDetailsEndpoint);
-        return foodDetails.run(
-                () -> restTemplate.getForObject(foodDetailsEndpoint, FoodDetailDto.class, barcode),
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(getAccessToken());
+
+        HttpEntity<Void> request = new HttpEntity<>(headers);
+
+        ResponseEntity<FoodDetailDto> response = foodDetails.run(
+                () -> restTemplate.exchange(foodDetailsEndpoint, HttpMethod.GET, request, FoodDetailDto.class, barcode),
                 throwable -> {
                     throw new FoodDetailsNotAvailableException("Food details not available");
                 }
         );
+
+        return response.getBody();
     }
 
     @Override
     public Float getKcalConsumed(String barcode, float quantity) {
         CircuitBreaker kcalConsumed = circuitBreakerFactory.create("kcalConsumed");
         LOGGER.info("Calling food details integrator BE with endpoint: " + kcalConsumedEndpoint);
-        return kcalConsumed.run(
-                () -> restTemplate.getForObject(kcalConsumedEndpoint, Float.class, barcode, quantity),
-                throwable -> {throw new FoodDetailsNotAvailableException("Food details not available");}
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(getAccessToken());
+
+        HttpEntity<Void> request = new HttpEntity<>(headers);
+
+        ResponseEntity<Float> response = kcalConsumed.run(
+                () -> restTemplate.exchange(kcalConsumedEndpoint, HttpMethod.GET, request, Float.class, barcode, quantity),
+                throwable -> {
+                    throw new FoodDetailsNotAvailableException("Food details not available");
+                }
         );
+
+        return response.getBody();
+    }
+
+    private String getAccessToken() {
+        String accessToken = "";
+        try {
+            GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
+            credentials.refreshIfExpired();
+            accessToken = credentials.getAccessToken().getTokenValue();
+        } catch (IOException e) {
+            LOGGER.error("Error while getting access token", e);
+        }
+        return accessToken;
     }
 
 }
