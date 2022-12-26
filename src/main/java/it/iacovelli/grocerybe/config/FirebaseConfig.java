84693c94976de319f1344298e9c9d33b494c4501
Nicola Iacovelli <nicolaiacovelli98@gmail.com>
2022-12-26 18:54:37 +0100

    Initialized firebase with config ⚒️

--- src/main/java/it/iacovelli/grocerybe/config/FirebaseConfig.java
+++ src/main/java/it/iacovelli/grocerybe/config/FirebaseConfig.java
@@ -3,6 +3,7 @@ package it.iacovelli.grocerybe.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,10 +12,14 @@ import java.io.IOException;
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${spring.cloud.gcp.project-id}")
+    private String firebaseProjectId;
+
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.getApplicationDefault())
+                .setProjectId(firebaseProjectId)
                 .build();
         return FirebaseApp.initializeApp(options);
     }
